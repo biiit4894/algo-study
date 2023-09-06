446ca17ad752b794048b22b47fdf928c0914dccc
biiit4894 <ghiesf@naver.com>
2023-09-06 17:30:27 +0900

    [3 숫자 카드 게임] 최소값 중 최대값 비교 코드 추가

--- chap3-greedy/3.py
+++ chap3-greedy/3.py
@@ -4,7 +4,10 @@ n, m = map(int, input().split())
 result = 0
 for i in range(n):
     data = list(map(int, input().split()))
+    min_value = 0
     for j in range(m):
         if data[j] <= data[j - 1]:
-            result = data[j]
+            min_value = data[j]
+    if(min_value >= result):
+        result = min_value
 print(result)

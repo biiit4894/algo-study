6ee7babff748e48aa59ae7bc1c1778d493d1b1ff
biiit4894 <ghiesf@naver.com>
2023-09-14 17:38:38 +0900

    [4 - 1이 될 때까지]N이 큰 수일때 풀이 추가

--- chap3-greedy/4.py
+++ chap3-greedy/4.py
@@ -1,23 +1,24 @@
-# # 단순하게 푸는 답안 예시
-# n, k = map(int, input().split())
-# result = 0
-#
-# # N이 K 이상이라면 K로 계속 나누기
-# while n >= k:
-#     # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
-#     while n % k != 0:
-#         n -= 1
-#         result += 1
-#     # K로 나누기
-#     n //= k
-#     result += 1
-#
-# while n > 1:
-#     n -= 1
-#     result += 1
-#
-# print(result)
+# 단순하게 푸는 답안 예시
+n, k = map(int, input().split())
+result = 0
 
+# N이 K 이상이라면 K로 계속 나누기
+while n >= k:
+    # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+    while n % k != 0:
+        n -= 1
+        result += 1
+    # K로 나누기
+    n //= k
+    result += 1
+
+while n > 1:
+    n -= 1
+    result += 1
+
+print(result)
+
+# 한꺼번에 1씩 빼는 효율적인 코드 예시
 # N, K를 공백으로 구분하여 입력받기
 n, k = map(int, input().split())
 result = 0
@@ -25,22 +26,18 @@ result = 0
 while True:
     # (N == K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
     target = (n // k) * k
-    print("target: ", target)
     result += (n - target)
     n = target
     # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
     if n < k:
         break
     # K로 나누기
-    print(result)
     result += 1
     n //= k
-    print(result)
 
 
 
 # 마지막으로 남은 수에 대하여 1씩 빼기
-print(result)
 result += (n - 1)
 
 print(result)

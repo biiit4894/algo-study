c7e2ad36abf18918295ebf640b16c7e3ee868884
biiit4894 <ghiesf@naver.com>
2023-11-30 15:38:21 +0900

    [1316] 그룹 단어 체커

---
+++ boj-intensive/1316.py
@@ -0,0 +1,15 @@
+N = int(input())
+count = N
+
+for i in range(N):
+  word = input()
+  for j in range(len(word) - 1):
+    if word[j] == word[j+1]:
+      continue
+    elif word[j] in word[j+1:]:
+      count -= 1
+      break
+
+print(count)
+      
+

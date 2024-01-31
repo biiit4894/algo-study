f5da44ccac31a09133849af68b66f6fa16410161
biiit4894 <ghiesf@naver.com>
2024-01-31 23:16:18 +0900

    [연습문제] 피보나치 수

---
+++ programmers/lv-2/FibonacciNumber.java
@@ -0,0 +1,16 @@
+class FibonacciNumber {
+    public int solution(int n) {
+        int answer = 0;
+        int a = 0;
+        int b = 1;
+
+        
+        for(int i = 2; i <= n; i++) {
+            answer = (a + b) % 1234567;
+            a = b;
+            b = answer;
+        }
+        
+        return answer;
+    }
+}

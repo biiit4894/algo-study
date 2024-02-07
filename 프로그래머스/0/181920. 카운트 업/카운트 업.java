1ba4f8b0ee16bc1c52d2ab7daaeadf1e5b47135a
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-02-07 17:32:57 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,12 +1,9 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        
-        int[] answer = new int[end_num - start_num + 1];
-        
-        int j = start_num;
-        for(int i = 0; i < end_num - start_num + 1; i++) {
-            answer[i] = j;
-            j++;
+        int arr_len = end_num - start_num + 1;
+        int[] answer = new int[arr_len];
+        for(int i = 0; i < arr_len; i++) {
+            answer[i] = start_num + i;
         }
         return answer;
     }

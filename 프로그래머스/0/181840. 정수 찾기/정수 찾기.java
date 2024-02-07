d063eb2c860005df9a13d622a4a999614b838b4b
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-02-07 17:19:44 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
         for(int num : num_list) {
-            if(num == n) {
-                answer = 1;
-                break;
-            }
+            if(num == n) return 1;
         }
-        return answer;
+        return 0;
     }
 }

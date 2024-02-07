5d532fafa9b67f1189522128d53fe2b7131b9217
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-02-07 17:13:00 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,19 +2,14 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
         
-        int num_1 = num_list[num_list.length - 1];
-        int num_2 = num_list[num_list.length - 2];
+        int last_num = num_list[num_list.length - 1] > num_list[num_list.length - 2] ?
+           num_list[num_list.length - 1] - num_list[num_list.length - 2] : num_list[num_list.length - 1] * 2;
         
         for(int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
         }
         
-        if(num_1 > num_2) {
-            answer[num_list.length] = num_1 - num_2;
-        } else {
-            answer[num_list.length] = num_1 * 2;
-        }
-
+        answer[num_list.length] = last_num;
         return answer;
     }
 }

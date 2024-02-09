e3726412988c9dfc024a1beea99e92052dbf9453
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-02-09 23:47:14 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.04 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string, int n) {
-        char[] answer = new char[n];
-        for(int i = 0; i < n; i++) {
-            answer[i] = my_string.charAt(i);
-        } 
-        return String.valueOf(answer);
+        return my_string.substring(0, n);
     }
 }

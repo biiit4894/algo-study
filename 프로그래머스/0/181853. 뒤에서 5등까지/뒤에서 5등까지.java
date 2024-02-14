c633359b8c6281914559ca7f337f23885acf516b
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-02-14 22:43:35 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.49 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
+        int[] answer = new int[5];
         Arrays.sort(num_list);
-        answer = Arrays.copyOfRange(num_list, 0, 5);
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[i];
+        }
         return answer;
     }
 }

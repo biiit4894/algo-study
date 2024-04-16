d71e65eadc0af598569c757e6796005fac97cece
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-04-16 17:11:08 +0900

    [level 2] Title: 더 맵게, Time: 1820.48 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+import java.lang.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for(int s : scoville) {
+            minHeap.add(s);
+        }
+        
+        while(minHeap.peek() < K) {
+            if(minHeap.size() == 1) {
+                return -1;
+            } else {
+                int first = minHeap.poll();
+                int second = minHeap.poll();
+                int mixed = first + second * 2;
+                minHeap.add(mixed);
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}

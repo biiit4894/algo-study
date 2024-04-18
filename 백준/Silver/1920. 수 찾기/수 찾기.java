4472accfd02bd9bc0ab9ec1a7ddb4a45fa2544a9
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-04-18 15:13:44 +0900

    [Silver IV] Title: 수 찾기, Time: 1960 ms, Memory: 187516 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,46 @@
 import java.util.Scanner;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Arrays;
 
-// BOJ 1920. 수 찾기
 public class Main {
+    private static boolean binarySearch(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length - 1;
+		while(left <= right) {
+			int mid = (left + right) / 2;
+			if(target == arr[mid]) {
+				return true;
+			} else if(target > arr[mid]) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		int n = scanner.nextInt();
-		HashSet<Integer> set1 = new HashSet<>();
+		int[] arr1 = new int[n];
 
 		for(int i = 0; i < n; i++) {
-			int num1 = scanner.nextInt();
-			set1.add(num1);
+			arr1[i] = scanner.nextInt();
 		}
 
 		int m = scanner.nextInt();
-		int[] set2 = new int[m];
-
+		int[] arr2 = new int[m];
 		for(int i = 0; i < m; i++) {
-			int num2 = scanner.nextInt();
-			set2[i] = num2;
+			arr2[i] = scanner.nextInt();
 		}
 
-		for (int i : set2) {
-			if (set1.contains(i)) {
-				System.out.println("1");
+		Arrays.sort(arr1);
+
+		for(int i = 0; i < m; i++) {
+			if(binarySearch(arr1, arr2[i])) {
+				System.out.println(1);
 			} else {
-				System.out.println("0");
+				System.out.println(0);
 			}
 		}
 	}

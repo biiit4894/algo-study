1bc63dc2348a6d67ae50e651ba93dd4ff808a08f
Hanbit <ghiesf@naver.com>
2024-04-24 22:25:08 +0900

    섹션 1. String(문자열) - 4. 단어 뒤집기

--- lecture/section1/4. 단어 뒤집기/Main2.java
+++ lecture/section1/4. 단어 뒤집기/Main2.java
@@ -1,9 +1,24 @@
 public class Main2 {
+	// 직접 뒤집는 풀이
 	public ArrayList<String> solution(int n, String[] str) {
 		ArrayList<String> answer = new ArrayList<>();
+		for (String x : str) {
+			char[] s = x.toCharArray();
+			int lt = 0, rt = x.length() - 1;
+			while (lt < rt) {
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
+			}
+			String tmp = String.valueOf(s); // 객체(x) 클래스.valueOf -> static method
+			answer.add(tmp);
+		}
+		return answer;
 	}
 	public static void main(String[] args) {
-		Main T = new Main();
+		Main2 T = new Main2();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		String[] str = new String[n];

38a0954c8514bcf600864be2c9d77bdf121f71f9
Hanbit <ghiesf@naver.com>
2024-06-17 23:11:56 +0900

    섹션6. Sorting and Searching(정렬, 이분검색과 결정알고리즘) - 3. 삽입정렬

---
+++ lecture/section6/3. 삽입정렬/InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class InsertionSort {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        InsertionSort T = new InsertionSort();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}

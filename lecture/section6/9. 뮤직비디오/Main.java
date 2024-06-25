c61f5243d02eed7b047cad1a55dba19012135c42
Hanbit <ghiesf@naver.com>
2024-06-25 22:20:11 +0900

    섹션6. Sorting and Searching(정렬, 이분검색과 결정알고리즘) - 9. 뮤직비디오

---
+++ lecture/section6/9. 뮤직비디오/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+// 정답 풀이
+class Main {
+    public int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0; // DVD 장수, DVD에 담아내는 곡들의 합
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt(); // 기본형 int를 리턴하지 않음..
+        int rt = Arrays.stream(arr).sum(); // 기본형 int를 리턴함
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
+}

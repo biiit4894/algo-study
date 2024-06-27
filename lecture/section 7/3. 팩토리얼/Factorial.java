9b001aa1ac590fe036e6c3b61051bbc372c31c35
Hanbit <ghiesf@naver.com>
2024-06-28 00:44:26 +0900

    섹션7. Recursive, Tree, Graph(DFS, BFS 기초) - 3. 팩토리얼

---
+++ lecture/section 7/3. 팩토리얼/Factorial.java
@@ -0,0 +1,16 @@
+// 나의 풀이
+public class Factorial {
+    public int DFS(int n) {
+        if(n == 0){
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        } else {
+            return n * DFS(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Factorial T = new Factorial();
+        System.out.println(T.DFS(5));
+    }
+}

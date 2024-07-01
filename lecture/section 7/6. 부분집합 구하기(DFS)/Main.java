a777db4fb4b282515d7bc649b902c6dcaeda86d8
Hanbit <ghiesf@naver.com>
2024-07-01 16:07:20 +0900

    섹션7. Recursive, Tree, Graph(DFS, BFS 기초) - 6. 부분집합 구하기(DFS)

---
+++ lecture/section 7/6. 부분집합 구하기(DFS)/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+        if (L == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    tmp += (i + " ");
+                }
+            }
+            if (tmp.length() > 0) {
+                System.out.println(tmp);
+            }
+        } else {
+            ch[L] = 1;
+            DFS(L + 1); // 왼쪽 노드
+            ch[L] = 0; // 해당 원소는 사용하지 않는다는 의미
+            DFS(L + 1); // 오른쪽 노드
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        n = 3;
+        ch = new int[n + 1];
+        T.DFS(1);
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+    }
+}

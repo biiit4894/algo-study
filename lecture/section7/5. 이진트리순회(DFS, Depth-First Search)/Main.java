74e4b6d247c2e74735bb81719691156c21dc08f4
biiit4894 <ghiesf@naver.com>
2024-09-01 12:28:11 +0900

    섹션10. 그리디 알고리즘 - 2. 회의실 배정

--- lecture/section7/5. 이진트리순회(DFS, Depth-First Search)/Main.java
+++ lecture/section7/5. 이진트리순회(DFS, Depth-First Search)/Main.java
@@ -1,6 +1,6 @@
 class Node {
     int data;
-    Node lt, rt;
+    Node lt, rt; // 노드 클래스의 객체 주소를 저장하는 변수
 
     public Node(int val) {
         data = val;
@@ -11,7 +11,7 @@ public class Main {
     Node root;
 
     public void DFS(Node root) {
-        if (root == null) {
+        if (root == null) { // 말단 노드로 온 것
             return;
         } else {
             //System.out.print(root.data + " "); // 전위 순회, 1 2 4 5 3 6 7
@@ -25,14 +25,14 @@ public class Main {
 
     public static void main(String[] args) {
         Main tree = new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
+        tree.root = new Node(1); // 객체가 생성되는 순간에는 data 값은 있지만 lt, rt가 모두 null이다.
+        tree.root.lt = new Node(2); // lt, rt 변수에 새로운 Node 객체를 생성한다.
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
-        tree.DFS(tree.root); // 1번 데이터를 가지고 있는 노드(루트 노드)
+        tree.DFS(tree.root); // 1번 데이터를 가지고 있는 노드(루트 노드, 100번지)
 
     }
 }
--- lecture/section7/6. 부분집합 구하기(DFS)/Main.java
+++ lecture/section7/6. 부분집합 구하기(DFS)/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static int[] ch;
 
     public void DFS(int L) {
-        if (L == n + 1) {
+        if (L == n + 1) { // 종학점에 도착했을 때
             String tmp = "";
             for (int i = 1; i <= n; i++) {
                 if (ch[i] == 1) {
@@ -15,9 +15,9 @@ public class Main {
             if (tmp.length() > 0) {
                 System.out.println(tmp);
             }
-        } else {
-            ch[L] = 1;
-            DFS(L + 1); // 왼쪽 노드
+        } else { // 종착점에 도달하지 못했을 때 -> 트리 두 갈래로 뻗어나가야 한다
+            ch[L] = 1; // 해당 원소는 사용한다는 의미
+            DFS(L + 1); // 왼쪽 노드 -> 사용한다는 의미에서 뻗는 함수
             ch[L] = 0; // 해당 원소는 사용하지 않는다는 의미
             DFS(L + 1); // 오른쪽 노드
         }
